399452f73941742b2bb0d194d1ef8583206dbc0a
viktoriya <dzherommo@gmail.com>
2024-01-27 12:21:14 +0300

    add clear cookies

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -40,6 +41,11 @@ public class TestBase {
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
+    }
+
+    @AfterEach
+    void clearBrowserCookies() {
+        Selenide.clearBrowserCookies();
     }
 }
 
